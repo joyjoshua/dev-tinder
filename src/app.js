f5a47b1461359200b638655d63d1b03d4eb3b896
joyjoshua <125115930+joyjoshua@users.noreply.github.com>
2025-08-28 21:55:22 +0530

    added error handling to the routes

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ const app = express();
 
 app.use('/admin', authAdmin);
 app.get('/admin/fetchUsers', (req, res) => {
-    res.send('User List is sent successfully')
+    res.send('User List is sent successfully');
 });
 app.delete('/admin/deleteUser', (req, res) => {
     res.send('user deleted successfully');
@@ -17,6 +17,16 @@ app.get('/user/fetchUserDetails', userAuth, (req, res) => {
 app.post('/user/login', (req, res) => {
     res.send('User logged in successfully');
 });
+
+app.get('/admin/replicateError', (req, res) => {
+    throw new Error('replicaating error');
+});
+
+app.use("/", (err, req, res, next) => {
+    if(err) {
+        res.status(500).send('encountered error, bro. Handled using err in request handler');
+    }
+})
 
 app.listen(3000, ()=> {
     console.log('Server is running at PORT 3000');
