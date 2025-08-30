9d403f7a7bf90d97eb1af7edab1b1208662ac41a
joyjoshua <125115930+joyjoshua@users.noreply.github.com>
2025-08-30 12:39:55 +0530

    db user schema, model added. Mock POST request created

--- src/app.js
+++ src/app.js
@@ -1,6 +1,27 @@
 const express = require('express');
 const app = express();
 const {connectDB} = require('./config/database');
+const {User} = require('./models/user');
+
+app.post('/signup', async (req, res) => {
+    const mockUser = {
+        firstName: 'joy',
+        lastName: 'joshua',
+        email: 'joyjoshua@gmail.com',
+        password: 'joy@1234',
+        age: 24,
+        gender: 'M'
+    };
+
+    const user = new User(mockUser);
+
+    try {
+        await user.save();
+        res.send("User added successfully");
+    } catch (err) {
+        res.status(400).send('Error while creating an error', err.message);
+    }
+})
 
 connectDB().then(()=> {
     console.log('DB connection established');
